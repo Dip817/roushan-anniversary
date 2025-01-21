3fa5eb393ce863c4a7a47f4361267be529f7646d
Diptanshu Dey <diptanshudey4@gmail.com>
2025-01-21 13:24:27 +0530

    New anniversary for roushan

---
+++ src/App.js
@@ -0,0 +1,59 @@
+import React, { useState } from "react";
+import "./App.css";
+
+const App = () => {
+  const [gifSrc, setGifSrc] = useState(
+    "https://media.tenor.com/Vy46BTSo3hsAAAAC/bear-love.gif"
+  );
+  const [message, setMessage] = useState("Go out with me?");
+  const [noButtonStyle, setNoButtonStyle] = useState({});
+
+  const maxHeight = window.innerHeight;
+  const maxWidth = window.innerWidth;
+
+  const getRandomNumber = (min, max) => {
+    return Math.floor(Math.random() * (max - min) + min);
+  };
+
+  const handleYesClick = () => {
+    setGifSrc("https://media.tenor.com/KFkmX5T_5LEAAAAC/love-you-brown-bear.gif");
+    setMessage("Yayyy!");
+  };
+
+  const handleNoClick = () => {
+    setGifSrc("https://media.tenor.com/jsQumF2np4kAAAAC/bear-shocked.gif");
+    setMessage("HOW THE FUCK, but anyways, now marry me");
+  };
+
+  const handleNoHover = () => {
+    setNoButtonStyle({
+      transform: `translate(${getRandomNumber(-maxWidth / 5, maxWidth / 5)}px, ${getRandomNumber(
+        -maxHeight / 5,
+        maxHeight / 5
+      )}px)`,
+    });
+  };
+
+  return (
+    <div className="main">
+      <img className="gif" src={gifSrc} alt="bear gif" />
+      <h3>{message}</h3>
+      <div className="btn-group">
+        <div className="btn" id="yes" onClick={handleYesClick}>
+          <p>YES</p>
+        </div>
+        <div
+          className="btn floating"
+          id="no"
+          onClick={handleNoClick}
+          onMouseEnter={handleNoHover}
+          style={noButtonStyle}
+        >
+          <p>NO</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
